359b9b4019706d8f84e1dc84dcb5b6e9eadbef94
Stefan Armbruster <stefan@armbruster-it.de>
2024-04-05 17:54:29 +0200

    fixes #4: atag.text.import.xml

--- src/main/java/atag/text/Importer.java
+++ src/main/java/atag/text/Importer.java
@@ -1,6 +1,7 @@
 package atag.text;
 
 import atag.util.ResultTypes;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.neo4j.graphdb.*;
@@ -9,7 +10,23 @@ import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Mode;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class Importer {
@@ -80,5 +97,96 @@ public class Importer {
                 throw new IllegalArgumentException("Unknown node type: " + node);
         }
         return index;
+    }
+    @Procedure(mode = Mode.WRITE, name = "atag.text.import.xml")
+    public Stream<ResultTypes.NodeResult> importXml(
+            @Name("startNode") Node startNode,
+            @Name("propertyKey") String propertyKey,
+            @Name(value = "xpath expression", defaultValue = "/TEI/text/body//node()") String path,
+            @Name(value = "label for annotation nodes", defaultValue = "Annotation") String labelString,
+            @Name(value = "property name for plain text", defaultValue = "plainText") String plainTextProperty,
+            @Name(value = "relationship type", defaultValue = "HAS_ANNOTATION") String relationshipTypeString ) {
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setNamespaceAware(true);
+            documentBuilderFactory.setIgnoringElementContentWhitespace(true);
+            documentBuilderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            documentBuilder.setEntityResolver((publicId, systemId) -> new InputSource(new StringReader("")));
+
+            String xml = (String) startNode.getProperty(propertyKey);
+            org.w3c.dom.Document doc = documentBuilder.parse(new ByteArrayInputStream(xml.getBytes()));
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+
+            XPath xPath = xPathFactory.newXPath();
+
+            path = StringUtils.isEmpty(path) ? "/" : path;
+            XPathExpression xPathExpression = xPath.compile(path);
+            NodeList nodeList = (NodeList) xPathExpression.evaluate(doc, XPathConstants.NODESET);
+
+            RelationshipType relationshipType = RelationshipType.withName(relationshipTypeString);
+            Label label = Label.label(labelString);
+            StringBuilder plainTextBuilder = new StringBuilder();
+
+            List<Node> annotations = new ArrayList<>();
+
+            for (int i=0; i<nodeList.getLength(); i++) {
+                org.w3c.dom.Node item = nodeList.item(i);
+//                int depth = getDepth(item);
+                switch (item) {
+                    case org.w3c.dom.Element element:
+                        Node newNeo4jNode = tx.createNode(label);
+                        annotations.add(newNeo4jNode);
+                        String textContent = element.getTextContent();
+                        startNode.createRelationshipTo(newNeo4jNode, relationshipType);
+                        newNeo4jNode.setProperty("tag", element.getNodeName());
+                        newNeo4jNode.setProperty("startIndex", Integer.toUnsignedLong(plainTextBuilder.length()));
+                        newNeo4jNode.setProperty("endIndex", Integer.toUnsignedLong(plainTextBuilder.length()+textContent.length()));
+                        Map<String, Object> attributes = getAttributes(element);
+                        attributes.forEach(newNeo4jNode::setProperty);
+                        if (!textContent.isEmpty()) {
+                            newNeo4jNode.setProperty(plainTextProperty, textContent);
+                        }
+                        if (log.isDebugEnabled()) {
+                            log.debug("{} Element: {}, attributes: {}, startIndex: {}, stopIndex: {}",
+                                    ".".repeat(getDepth(item)), element.getNodeName(), attributes,
+                                    plainTextBuilder.length(), plainTextBuilder.length()+textContent.length());
+                        }
+                        break;
+                    case org.w3c.dom.Text text:
+                        if (log.isDebugEnabled()) {
+                            log.debug( "{} Text: {}",".".repeat(getDepth(item)), text.getTextContent());
+                        }
+                        plainTextBuilder.append(text.getTextContent());
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown node type: " + item);
+                }
+            }
+            String plainText = plainTextBuilder.toString();
+            startNode.setProperty(plainTextProperty, plainText);
+            return annotations.stream().map(ResultTypes.NodeResult::new);
+        } catch (ParserConfigurationException | XPathExpressionException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Map<String,Object> getAttributes(Element element) {
+        NamedNodeMap attributes = element.getAttributes();
+        Map<String,Object> result = new HashMap<>();
+        for (int i=0; i<attributes.getLength(); i++) {
+            org.w3c.dom.Node item = attributes.item(i);
+            result.put(item.getNodeName(), item.getNodeValue());
+        }
+        return result;
+    }
+
+    private int getDepth(org.w3c.dom.Node item) {
+        return getDepth(item, 0);
+    }
+
+    private int getDepth(org.w3c.dom.Node item, int depth) {
+        org.w3c.dom.Node parent = item.getParentNode();
+        return parent==null ? depth : getDepth(parent, depth+1);
     }
 }
--- src/test/java/atag/text/ImporterTest.java
+++ src/test/java/atag/text/ImporterTest.java
@@ -1,27 +1,59 @@
 package atag.text;
 
+import org.apache.commons.io.IOUtils;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.harness.junit.extension.Neo4jExtension;
 import org.neo4j.internal.helpers.collection.Iterators;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ImporterTest {
 
+/*    @SystemStub
+    static private EnvironmentVariables environmentVariables = new EnvironmentVariables()
+            .set("apoc.import.file.enabled", "true")
+            .set("apoc.import.file.use_neo4j_config", "false");
+
     @RegisterExtension
+    @Order(1)
+    static SystemStubsExtension systemStubsExtension = new SystemStubsExtension();*/
+
+    @RegisterExtension
+    @Order(2)
     static Neo4jExtension neo4j = Neo4jExtension.builder()
             .withDisabledServer()
-            .withFixture("CREATE (t:Text {id:1, text: 'This <em>is</em> a <em>emphasized test with a <a href=\\'ref\\'>link</a></em>. We also have a <br/> line break.'})")
+            .withFixture("""
+            CREATE (t:Text {id:1, text: 'This <em>is</em> a <em>emphasized test with a <a href=\\'ref\\'>link</a></em>. We also have a <br/> line break.'})
+            """)
+            .withFixture(db -> {
+                        try {
+                            String xml = IOUtils.toString(ImporterTest.class.getResourceAsStream("/patzig.xml"), StandardCharsets.UTF_8);
+                            db.executeTransactionally("""
+                                    CREATE (t:Text {id:2, text:$xml})
+                                    """, Map.of("xml", xml));
+                            return null;
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            )
             .withProcedure(Importer.class)
+//            .withProcedure(Xml.class)
             .build();
 
     @Test
@@ -69,4 +101,36 @@ public class ImporterTest {
                 Collections.emptyMap(), result -> Iterators.single(result).get("plainText").toString());
         assertEquals("This is a emphasized test with a link. We also have a  line break.", plainText);
     }
+
+    @Test
+    public void testAnnotateXml(GraphDatabaseService db) throws IOException {
+        String xml = IOUtils.toString(Objects.requireNonNull(this.getClass().getResourceAsStream("/patzig.xml")), UTF_8);
+        db.executeTransactionally("""
+                MATCH (t:Text{id: 2})
+                CALL atag.text.import.xml(t, 'text') YIELD node
+                RETURN properties(node) as node
+                """, Map.of("xml", xml), result -> {
+            ResourceIterator<Object> node = result.columnAs("node");
+            List<Object> list = node.stream().toList();
+
+//            list.forEach(System.out::println);
+            assertEquals(70, list.size());
+
+            assertThat((Map<String, Object>) list.get(63), Matchers.<Map<String, Object>>allOf(
+                    hasEntry("startIndex", 1029L),
+                    hasEntry("endIndex", 1045L),
+                    hasEntry("tag", "choice"),
+                    hasEntry("plainText", "TrabantTrabanten")
+            ));
+            return null;
+        });
+
+        db.executeTransactionally("MATCH (t:Text{id: 2}) RETURN t.plainText as plainText",
+                Collections.emptyMap(), result -> {
+                    String plainText = Iterators.single(result).get("plainText").toString();
+                    assertEquals("TrabantTrabanten", plainText.substring(1029, 1045));
+                    return null;
+                });
+    }
+
 }
