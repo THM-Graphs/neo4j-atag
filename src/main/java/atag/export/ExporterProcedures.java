28cddb3330e06f37ca6837fa566481c9f4c97bd3
Stefan Armbruster <stefan@armbruster-it.de>
2025-08-18 09:20:26 +0200

    feat: add ExporterProcedures for exporting graphs to JGF format with corresponding tests

---
+++ src/main/java/atag/export/ExporterProcedures.java
@@ -0,0 +1,86 @@
+package atag.export;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.neo4j.graphdb.Entity;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.internal.helpers.collection.Iterators;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.UserFunction;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ExporterProcedures {
+
+    @UserFunction
+    @Description("export a graph into JGF format")
+    public String exportJgf(@Name("nodes") List<Node> nodes, @Name("relationships") List<Relationship> relationships) {
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode root = mapper.createObjectNode();
+            ObjectNode graph = root.putObject("graph");
+
+
+            //root.put("graph", "exported_graph");
+            graph.put("label", OffsetDateTime.now().toString());
+//            graph.put("type", "graph");
+            //root.put("label", "Neo4j Export");
+            graph.put("directed", true);
+
+            ObjectNode jsonNodes = graph.putObject("nodes");
+            ArrayNode jsonRelationships = graph.putArray("edges");
+
+            for (Node node : nodes) {
+                ObjectNode jsonNode = jsonNodes.putObject(node.getElementId());
+/*
+                // use json array for labels - note compliant with JGF spec
+                ArrayNode labels = jsonNode.putArray("label");
+                node.getLabels().forEach(l-> labels.add(l.name()));
+*/
+                jsonNode.put("label", Iterators.stream(node.getLabels().iterator()).map(Label::name).collect(Collectors.joining(",")));
+                addPropertiesToJsonNode(node, jsonNode);
+            }
+
+            for (Relationship rel : relationships) {
+                ObjectNode edgeObj = jsonRelationships.addObject();
+                edgeObj.put("source", rel.getStartNode().getElementId());
+                edgeObj.put("target", rel.getEndNode().getElementId());
+                edgeObj.put("relation", rel.getType().name());
+                addPropertiesToJsonNode(rel, edgeObj);
+
+            }
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addPropertiesToJsonNode(Entity entity, ObjectNode jsonNode) {
+        Map<String, Object> allProperties = entity.getAllProperties();
+        if (!allProperties.isEmpty()) {
+            ObjectNode metadata = jsonNode.putObject("metadata");
+            allProperties.forEach((key, value) -> {
+                switch (value) {
+                    case String str -> metadata.put(key, str);
+                    case Integer i -> metadata.put(key, i);
+                    case Long l -> metadata.put(key, l);
+                    case Double d -> metadata.put(key, d);
+                    case Boolean b -> metadata.put(key, b);
+                    case LocalDate d -> metadata.put(key, d.toString());
+                    default -> throw new IllegalArgumentException("Unsupported property type: " + value.getClass().getName());
+                }
+            });
+        }
+    }
+
+}
---
+++ src/test/java/atag/export/ExporterProceduresTest.java
@@ -0,0 +1,58 @@
+package atag.export;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.harness.junit.extension.Neo4jExtension;
+import org.neo4j.internal.helpers.collection.Iterators;
+
+import javax.ws.rs.ProcessingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExporterProceduresTest {
+
+    @RegisterExtension
+    static Neo4jExtension neo4j = Neo4jExtension.builder()
+            .withFunction(ExporterProcedures.class)
+            .withDisabledServer()
+            .withFixture("""
+                CREATE (a:Person {name: 'Alice', dob: date({year: 2012, month: 6, day: 1}), height: 175})
+                CREATE (b:Person {name: 'Bob'})
+                CREATE (a)-[:KNOWS]->(b)
+                """)
+            .build();
+
+    @Test
+    void testExportToJson(GraphDatabaseService db) throws URISyntaxException, ProcessingException, JsonProcessingException {
+        // When
+        String json = db.executeTransactionally("""
+                MATCH (a)-[r]->(b)
+                WITH collect(a) + collect(b) AS nodes, collect(r) AS relationships
+                RETURN atag.export.exportJgf(nodes, relationships) AS json
+                """, Collections.emptyMap(), r -> Iterators.single(r).get("json").toString());
+        System.out.println(json);
+
+        final JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
+        URI uri = ExporterProceduresTest.class.getResource("/json-graph-schema.json").toURI();
+        final JsonSchema schema = factory.getSchema(uri);
+        JsonNode jsonNode = new ObjectMapper().readTree(json);
+        Set<ValidationMessage> validationMessages = schema.validate(jsonNode);
+
+        validationMessages.forEach(validationMessage -> System.out.println(validationMessage.getMessage()));
+
+        // Then
+        assertEquals(0, validationMessages.size(), "JSON does not conform to schema");
+    }
+}
